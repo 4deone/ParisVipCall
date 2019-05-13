291dc0b2224ca25d9ce21b184221f3d252e974dc
patrick <playbydeone@gmail.com>
2019-05-13 11:21:58 +0100

    Initial commit

---
+++ app/src/main/java/com/bydeone/parisvipcall/controllers/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bydeone.parisvipcall.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bydeone.parisvipcall.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
